f4140a30e961358a7d510d41242a4894d9e3dcbe
Kevenoliveiramartins <208955932+kevenoliveiramartins@users.noreply.github.com>
2026-03-16 17:28:58 -0300

    cria rota de cadastro

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
-import { Route, Routes } from 'react-router-dom'
+import {Form, Route, Routes} from 'react-router-dom'
 import Home from './pages/Home'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
+import Cadastro from "./pages/Cadastro.jsx";
+import Login from "./pages/Login.jsx";
 
 function App() {
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/login" element={<Login />} />
       </Routes>
       <Footer />
     </>

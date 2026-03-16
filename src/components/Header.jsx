69d341363d2cc1cc982957e55dc7e91c4d6af693
marcelasan123 <marcela.san0209@gmail.com>
2026-03-16 17:10:12 -0300

    cria componentes de botao

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,22 @@
 import logo from '../assets/logo.svg';
+import Vector from '../assets/Vector.png'
+import styles from './Header.module.css';
 
 export default function Header() {
     return (
-        <header className="container-fluid bg-black">
+        <header className="container-fluid bg-black d-flex">
             <div className="container py-2">
                 <img src={logo} width={100} alt="" />
+            </div>
+            <div className={styles["header-actions"] + " d-flex align-items-center justify-content-between"}>
+                <button className={styles["header-cadastrar"]}>
+                    CADASTRAR
+                </button>
+
+                <button className={styles["header-login"]}>
+                    LOGIN
+                    <img src={Vector} alt="login" className={styles["Vector"]} />
+                </button>
             </div>
         </header>
     )

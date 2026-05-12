8bc8b825425827b7529407441447375974eb5692
anagregolin <paulo.cavallini08@gmail.com>
2026-05-12 15:32:14 -0300

    fix: finaliza fluxo de criar reserva

--- src/components/ModalPix/ModalPix.jsx
+++ src/components/ModalPix/ModalPix.jsx
@@ -1,13 +1,14 @@
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useAsyncError, useNavigate } from 'react-router-dom';
 import estilo from './ModalPix.module.css';
 
-export default function ModalPix({ chavePix = "41317641809", valor, aoFechar }) {
+export default function ModalPix({ chavePix = "41317641809", valor, aoFechar, aoConfirmarPagamento, onErro }) {
     const [confirmando, setConfirmando] = useState(false);
     const [qrCodeUrl, setQrCodeUrl] = useState(null);
     const [carregandoQr, setCarregandoQr] = useState(true);
     const navigate = useNavigate();
     const [payload, setPayload] = useState(null);
+    const [textoCopiar, setTextoCopiar] = useState("Copiar");
 
     useEffect(() => {
         async function buscarQrCode() {
@@ -25,7 +26,7 @@ export default function ModalPix({ chavePix = "41317641809", valor, aoFechar }) 
 
                 const data = await res.json();
                 setQrCodeUrl(`data:image/png;base64,${data.qrcode}`);
-                setPayload(data.payload); // opcional, se quiser exibir
+                setPayload(data.payload);
             } catch (err) {
                 console.error("Erro ao gerar QR Code:", err);
             } finally {
@@ -36,12 +37,19 @@ export default function ModalPix({ chavePix = "41317641809", valor, aoFechar }) 
         buscarQrCode();
     }, [chavePix, valor]);
 
-    function handleConfirmar() {
+    async function handleConfirmar() {
         setConfirmando(true);
-        setTimeout(() => {
+        try {
+            await aoConfirmarPagamento();
+            setTimeout(() => {
+                aoFechar();
+                navigate('/');
+            }, 2000);
+        } catch (err) {
+            setConfirmando(false);
             aoFechar();
-            navigate('/');
-        }, 2000);
+            onErro?.(err.message || "Erro ao confirmar reserva. Tente novamente.");
+        }
     }
 
     return (
@@ -59,7 +67,7 @@ export default function ModalPix({ chavePix = "41317641809", valor, aoFechar }) 
                                 <img
                                     src={qrCodeUrl}
                                     alt="QR Code PIX"
-                                    style={{ width: "100%", height: "100%", borderRadius: "8px" }}
+                                    className="col-8 rounded-4"
                                 />
                             ) : (
                                 <p style={{ color: "white", textAlign: "center" }}>Erro ao gerar QR Code</p>
@@ -69,14 +77,26 @@ export default function ModalPix({ chavePix = "41317641809", valor, aoFechar }) 
                         {payload && (
                             <div className={estilo.pixInfo}>
                                 <span className={estilo.pixLabel}>Copia e Cola</span>
-                                <span
-                                    className={estilo.pixChave}
-                                    style={{ cursor: "pointer", wordBreak: "break-all" }}
-                                    onClick={() => navigator.clipboard.writeText(payload)}
-                                    title="Clique para copiar"
-                                >
-                                    {payload}
-                                </span>
+                                    <div className="d-flex flex-column gap-2">
+                                        <span
+                                        className={estilo.pixChave +  " rounded-3"}
+                                        style={{ cursor: "pointer", wordBreak: "break-all" }}
+                                        onClick={() => navigator.clipboard.writeText(payload)}
+                                        title="Clique para copiar"
+                                    >
+                                        {payload}
+                                    </span>
+                                    <button
+                                        onClick={() => {
+                                            navigator.clipboard.writeText(payload);
+                                            setTextoCopiar("Copiado!");
+                                            setTimeout(() => setTextoCopiar("Copiar"), 500)
+                                        }}
+                                        className="rounded-3 px-2 fw-semibold"
+                                    >
+                                        {textoCopiar}
+                                    </button>
+                                </div>
                             </div>
                         )}
 
@@ -94,6 +114,13 @@ export default function ModalPix({ chavePix = "41317641809", valor, aoFechar }) 
                             disabled={confirmando}
                         >
                             {confirmando ? 'Confirmando...' : 'Confirmar Pagamento'}
+                        </button>
+                        <button
+                            className={`${estilo.btnBase}`}
+                            onClick={aoFechar}
+                            disabled={confirmando}
+                        >
+                            Cancelar
                         </button>
                     </div>
                 </div>
